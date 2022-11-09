10f4269d82884c9f80085f655ce93bb766201cbe
jaybon1 <ponggas2@gmail.com>
2022-11-10 03:36:40 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Counter from "pages/Counter";
+import Main from "pages/Main";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Main />} />
+        <Route path={"/counter"} element={<Counter />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/styles/HeaderStyle.js
@@ -0,0 +1,17 @@
+// 타입힌트 방식
+
+/** @type {React.CSSProperties} */
+const HeaderSytle = {
+  backgroundColor: "yellow",
+};
+
+export default HeaderSytle;
+
+// /** @type { (color: string) => React.CSSProperties } */
+// const HeaderSytle = (color) => {
+//   return {
+//     backgroundColor: color,
+//   };
+// };
+
+// export default HeaderSytle;
